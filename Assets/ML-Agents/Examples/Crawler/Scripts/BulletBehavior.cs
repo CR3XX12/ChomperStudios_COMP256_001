2e1c12ac9ca5989c5e29ede85ee8954c0d23c322
CR3XX12 <crex71228@gmail.com>
2025-04-02 02:41:18 -0400

    Implemented Bullet and collides with enemy
    - Added script for Bullet to shoot
    - added trigger of the right hand controller to shoot trigger the bullet to shoot
    - Added Enemy Tag
    - Added Hitbox to enemies to collide with bullet and make enemy disappear

---
+++ Assets/ML-Agents/Examples/Crawler/Scripts/BulletBehavior.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+    public float lifetime = 5f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime); // Auto-destroy bullet after X seconds
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("[Bullet] Hit: " + other.gameObject.name);
+
+        // Start from what we hit
+        Transform crawler = other.transform;
+
+        // Go up the hierarchy until we find an object tagged "Enemy"
+        while (crawler != null && !crawler.CompareTag("Enemy"))
+        {
+            crawler = crawler.parent;
+        }
+
+        // If we found the enemy object, destroy it
+        if (crawler != null)
+        {
+            Debug.Log("[Bullet] Enemy hit! Destroying: " + crawler.name);
+            Destroy(crawler.gameObject);
+        }
+        else
+        {
+            Debug.Log("[Bullet] Hit something, but it wasn’t tagged 'Enemy'");
+        }
+
+        // Destroy the bullet itself after collision
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/ML-Agents/Examples/Crawler/Scripts/XRGunShoot.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class XRGunShoot : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform shootPoint;
+    public float bulletSpeed = 20f;
+
+    public InputActionProperty shootAction;
+
+    void Update()
+    {
+        if (shootAction.action.WasPressedThisFrame())
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, shootPoint.position, shootPoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        rb.velocity = shootPoint.forward * bulletSpeed;
+    }
+}
